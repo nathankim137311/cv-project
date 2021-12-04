3797b68566159421ca2f3dd8f45739cb0d8e4f45
web-dev <nathankim137311@gmail.com>
2021-12-03 21:54:07 -0800

    added states for all components and handleChange methods

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,6 +1,23 @@
 import { Component } from 'react'
 
 class Education extends Component { 
+    constructor() {
+        super()
+        this.state = {
+          university: '',
+          city: '',
+          degree: '',
+          startDate: '',
+          endDate: '', 
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+        console.log(this.state);
+    }
 
     render() {
         const stylingObject = {
@@ -15,21 +32,31 @@ class Education extends Component {
                     <h1>Education</h1>
                     <div className="form-control">
                         <label htmlFor="university">University</label>
-                        <input type="text" id="university" />
+                        <input type="text" id="university" name="university" 
+                        onChange={this.handleChange}
+                        />
                     </div>
                     <div className="form-control">
                         <label htmlFor="city">City</label>
-                        <input type="text" id="city"/>
+                        <input type="text" id="city" name="city"
+                        onChange={this.handleChange}
+                        />
                     </div>
                     <div className="form-control">
                         <label htmlFor="degree">Degree</label>
-                        <input type="text" id="degree"/>
+                        <input type="text" id="degree" name="degree"
+                        onChange={this.handleChange}
+                        />
                     </div>
                     <div className="form-control date">
                         <label htmlFor="start-date">Start</label>
-                        <input type="date" id="start-date" />
+                        <input type="date" id="start-date" name="startDate" 
+                        onChange={this.handleChange}
+                        />
                         <label htmlFor="end-date">End</label>
-                        <input type="date" id="end-date" />
+                        <input type="date" id="end-date" name="endDate"
+                        onChange={this.handleChange}
+                        />
                     </div>
                 </div>
                 <button style={stylingObject.button} className="submit-btn" type="submit">Create</button>
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,24 +1,49 @@
 import { Component } from 'react'
 
 class Experience extends Component {
+    constructor() {
+        super()
+        this.state = {
+          jobPosition: '',
+          companyName: '',
+          startDate: '',
+          endDate: '', 
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+        console.log(this.state);
+    }
+
     render() {
         return (
             <form id="form">
                 <div className="form-control-container">
                     <h1>Work Experience</h1>
                     <div className="form-control">
-                        <label htmlFor="position">Job position</label>
-                        <input type="text" id="position" />
+                        <label htmlFor="job-position">Job position</label>
+                        <input type="text" id="job-position" name="jobPosition" 
+                        onChange={this.handleChange}
+                        />
                     </div>
                     <div className="form-control">
                         <label htmlFor="company-name">Company</label>
-                        <input type="text" id="company-name" />
+                        <input type="text" id="company-name" name="companyName"
+                        onChange={this.handleChange}
+                        />
                     </div>
                     <div className="form-control date">
                         <label htmlFor="start-date">Start</label>
-                        <input type="date" id="start-date" />
+                        <input type="date" id="start-date" name="startDate"  
+                        onChange={this.handleChange}
+                        />
                         <label htmlFor="end-date">End</label>
-                        <input type="date" id="end-date" />
+                        <input type="date" id="end-date" name="endDate" 
+                        onChange={this.handleChange}
+                        />
                     </div>
                 </div>
                 <button className="submit-btn" type="submit">Next</button>
